1b8dfa0cefc81139789cda972714943ec74433bd
pshemski <p.markowski1980@gmail.com>
2016-08-17 12:53:49 +0700

    created like model and controller

---
+++ app/models/like.rb
@@ -0,0 +1,6 @@
+class Like < ActiveRecord::Base
+	validates :user_id, pesence: true
+	validates :status_id, presence: true
+	belongs_to :user
+	belongs_to :status
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-
+	resources :likes, only: [:new, :create, :index, :destroy]
 
 end
